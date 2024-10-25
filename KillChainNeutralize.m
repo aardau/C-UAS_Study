478708c8a9b80884a774b4acc7394de7260dbae2
timhe <timhe@172.28.0.184>
2024-10-25 14:16:22 -0400

    writing psuedo code

--- KillChainNeutralize.m
+++ KillChainNeutralize.m
@@ -1,5 +1,7 @@
 % time to kill kinetic
 % types of neutralization with DEWs
+    % different types have different ranges and different styles
 % time to neutralize with DEWs
+    % how long to activate, how long to disable
 % time accounting for weapon misses/error
 % 
