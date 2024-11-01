1c5ae9bfbf831e1092e9563e08acdbf3b4cca69a
stark <stark@172.28.142.203>
2024-11-01 13:10:00 -0400

    Comments giving basic thinking of how to set up the script

--- DetectionScript.m
+++ DetectionScript.m
@@ -1,4 +1,19 @@
 clc
 clear
 
-x = 2+2;
+%Set paramaters for fixed and mobile effectors
+    %Include field of view and range
+    %Assumed that mobile is worse than fixed 
+
+%Create statement for detection
+    %Pull adversary flight path
+    %If the object crosses into the field of view and range, it is detected
+    %If not, it is not detected
+    %Need to set it up so that each effector is active and can detect an
+    %object
+    %varies based on number and type of effectors
+
+%Outputs: p
+    %Positive detection notification
+    %Which effector detected the object to pass along to tracking
+    %
