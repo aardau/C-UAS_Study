b6d7be2f40637b915be508a6d5f3ed9af1e6cae7
timhe <timhe@172.28.9.14>
2024-11-01 13:32:26 -0400

    Updated psuedo code

--- KillChainNeutralize.m
+++ KillChainNeutralize.m
@@ -1,7 +1,20 @@
-% time to kill kinetic
-% types of neutralization with DEWs
-    % different types have different ranges and different styles
-% time to neutralize with DEWs
-    % how long to activate, how long to disable
+%Neglect the time it takes for projectile or energy to travel to target
+
 % time accounting for weapon misses/error
 % 
+%% timing
+% if projectile hits its target, the target will no longer be focused and
+% the target will fall for a time
+    %if projectile misses, fire again instantaneously
+
+% DEWs time to kill with high power microwaves or lasers (AOE vs Pinpoint
+% accuracy)(Fry circuits vs melt through)
+    % how long does energy need to be focused on target to neutralize?
+        % Laser: 10-60 seconds depending on how far away, how strong laser
+        % is, and how strong material is
+        %Microwaves: 1-10 seconds to fry circuits, depending on the same.
+    % range of weapons and how much Area of Effect
+    % if DEW runs out of power (especially for mobile effector), then it is
+    % disabled until further notice
+            % charge up time?
+    %continuous energy output until target is neutralized
