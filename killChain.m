a11e5ea022218d4308066eb7a67a7c7a5f92b895
richardk34 <158100907+richardk34@users.noreply.github.com>
2025-01-27 15:39:53 -0500

    adjusted func

--- killChain.m
+++ killChain.m
@@ -2,10 +2,10 @@
 %plan:
 % Take x,y from all UAS and test their collision for each static defense.
 
-function killData = killChain(x,y, mapFeatures)
+function killData = killChain(adversaryPosition, mapFeatures)
     for j = 1:size(mapFeatures.staticDefenses,1) %FOR EACH STATIC DEFENSE
         for i = 1:length(x) %FOR EACH POINT IN THE UAS FLIGHT PATH
-            distanceToUAS = sqrt((mapFeatures.staticDefenses(j,1)-x(i))^2 + (mapFeatures.staticDefenses(j,2)-y(i))^2);
+            distanceToUAS = sqrt((mapFeatures.staticDefenses(j,1)-adversaryPosition(1,i))^2 + (mapFeatures.staticDefenses(j,2)-adversaryPosition(2,i))^2);
     
             if distanceToUAS <= mapFeatures.staticDefenses(j,3)
                 disp("UAS intersect")
